e3a46cac49ec71f4e73a1b14ee3e9b4b36bfab32
Vinay Udyawer <vinay.udyawer@gmail.com>
2019-09-17 12:53:00 +0930

    Update 2019-09-17_CircuitousAnimation.R

--- R/Functions to be finished/2019-09-17_CircuitousAnimation.R
+++ R/Functions to be finished/2019-09-17_CircuitousAnimation.R
@@ -13,38 +13,51 @@ data("PointsCircuitous_crocs")
 array <- as_tibble(PointsCircuitous_crocs)
 
 data("crocs")
-dets <- 
-  crocs %>% 
-  as_tibble() %>% 
-  mutate(Date.Time = ymd_hms(Date.Time))
+dets <-
+  crocs %>%
+  as_tibble() %>%
+  left_join(array, by=c("Receiver.S.N" = "LOCATION")) %>%
+  mutate(Date.Time = ymd_hms(Date.Time)) %>%
+  arrange(ID, Date.Time) %>%
+  st_as_sf(coords=c("LONGITUDE", "LATITUDE"), crs=4326) %>%
+  group_by(ID) %>%
+  mutate(Distance.between.fixes.meters = st_distance(geometry[lag(row_number())], geometry, by_element=T)) %>%
+  as_Spatial() %>% as_tibble() %>%
+  rename(LONGITUDE = coords.x1,
+         LATITUDE = coords.x2)
 
-trajfun <- 
+detections <-
+  dets %>%
+  filter(Distance.between.fixes.meters > 0) %>%
+  dplyr::select(-c(RADIUS, LONGITUDE, LATITUDE))
+
+trajfun <-
   function(detections, array){
-    combdat <- 
-      detections %>% 
+    combdat <-
+      detections %>%
       left_join(array, by=c("Receiver.S.N" = "LOCATION"))
-    
+
     for(i in 1:nrow(combdat)){
       if(i %in% 1){out <- combdat %>% slice(1)}
-      
+
       tmpdf <- combdat %>% slice(i:(i+1))
       if(n_distinct(tmpdf$Receiver.S.N) > 1){
-        
-        waypoints <- 
-          array[which(grepl(tmpdf$Receiver.S.N[1], array$LOCATION)): which(grepl(tmpdf$Receiver.S.N[2], array$LOCATION)),] %>% 
+
+        waypoints <-
+          array[which(grepl(tmpdf$Receiver.S.N[1], array$LOCATION)): which(grepl(tmpdf$Receiver.S.N[2], array$LOCATION)),] %>%
           slice(-1, -nrow(.))
-        
+
         tmpdf <-
-          tmpdf %>% 
+          tmpdf %>%
           add_row(LONGITUDE = waypoints$LONGITUDE,
                   LATITUDE = waypoints$LATITUDE,
-                  .after = 1) %>% 
+                  .after = 1) %>%
           mutate(ID = first(ID),
                  Transmitter.Name = first(Transmitter.Name),
                  Transmitter.S.N = first(Transmitter.S.N),
                  Date.Time = seq(as.POSIXct(first(Date.Time)), as.POSIXct(last(Date.Time)), len = nrow(.)))
         out <- bind_rows(out, tmpdf)
-       
+
         } else {
           out <- bind_rows(out, tmpdf)
         }
@@ -53,13 +66,13 @@ trajfun <-
   }
 
 traj <-
-  dets %>% 
-  group_by(ID) %>% 
-  do(trajfun(., array)) %>% 
+  detections %>%
+  group_by(ID) %>%
+  do(trajfun(., array)) %>%
   filter(!duplicated(Date.Time, ID))
 
-traj %>% 
-  st_as_sf(coords=c("LONGITUDE", "LATITUDE"), crs = 4326) %>% 
+traj %>%
+  st_as_sf(coords=c("LONGITUDE", "LATITUDE"), crs = 4326) %>%
   mapview(zcol = "ID", burst = T)
 
 
@@ -68,6 +81,8 @@ movedat <- df2move(df = traj,
                   proj = CRS("+init=epsg:4326"),
                   x = "LONGITUDE", y = "LATITUDE",
                   time = "Date.Time", track_id = "ID")
+
+plot(movedat)
 
 # align move_data to a uniform time scale
 m <- align_move(movedat)
